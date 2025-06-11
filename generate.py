d04405f853daed5cdde8b68714b0401bc3f6aa3b
RishabhChandak22 <rishabhchandak22@gmail.com>
2025-06-11 12:06:30 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,1 +1,14 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+model_name = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+input_text = "Who is the greatest baseball player of all time?"
+
+inputs = tokenizer(input_text, return_tensors="pt")
+outputs = model.generate(**inputs, max_new_tokens=50)
+
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
